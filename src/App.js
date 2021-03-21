3c2d5478e70bbceb1d58e8a43ad59eb5cf44881c
hannah26hannah <ooyoonoo24@gmail.com>
2021-03-21 13:28:35 +0900

    :sparkles: React-router-dom extra features

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { Link, Route } from "react-router-dom";
+import { Link, Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import Profiles from "./Profiles";
+import HistorySample from "./HistorySample";
 
 const App = () => {
   return (
@@ -17,11 +18,28 @@ const App = () => {
           <li>
             <Link to="/profiles">Profile</Link>
           </li>
+          <li>
+            <Link to="/history">History Sample</Link>
+          </li>
         </ul>
       </nav>
-      <Route path="/" exact component={Home} />
-      <Route path={["/about", "/info"]} component={About} />
-      <Route path="/profiles" component={Profiles} />
+      <hr />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path={["/about", "/info"]} component={About} />
+        <Route path="/profiles" component={Profiles} />
+        <Route path="/history" component={HistorySample} />
+
+        <Route
+          // path를 따로 정의하지 않으면 모든 상황에 렌더링됨.
+          render={({ location }) => (
+            <div>
+              <h2>이 페이지는 존재하지 않습니다.</h2>
+              <p>{location.pathname}</p>
+            </div>
+          )}
+        />
+      </Switch>
     </main>
   );
 };
--- src/Profiles.js
+++ src/Profiles.js
@@ -1,17 +1,26 @@
 import React from "react";
-import { Link, Route } from "react-router-dom";
+import { NavLink, Route } from "react-router-dom";
 import Profile from "./Profile";
+import WithRouterSample from "./WithRouterSample";
 
 const Profiles = () => {
+  const activeStyle = {
+    background: "black",
+    color: "white",
+  };
   return (
     <div>
       <h3>사용자 목록</h3>
       <ul>
         <li>
-          <Link to="/profiles/hannah">Hannah</Link>
+          <NavLink activeStyle={activeStyle} to="/profiles/hannah">
+            Hannah
+          </NavLink>
         </li>
         <li>
-          <Link to="/profiles/hyunjeong">Hyunjeong</Link>
+          <NavLink activeStyle={activeStyle} to="/profiles/hyunjeong">
+            Hyunjeong
+          </NavLink>
         </li>
       </ul>
 
@@ -21,6 +30,7 @@ const Profiles = () => {
         render={() => <div>사용자를 선택해주세요</div>}
       />
       <Route path="/profiles/:username" component={Profile} />
+      <WithRouterSample />
     </div>
   );
 };
