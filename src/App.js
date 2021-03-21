b7f5d541c376f2c880dc4975f55334478eaa9a10
hannah26hannah <ooyoonoo24@gmail.com>
2021-03-21 12:25:56 +0900

    :recycle: with immer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useCallback, useRef, useState } from "react";
+import produce from "immer";
 
 const App = () => {
   const nextId = useRef(1);
@@ -15,10 +16,11 @@ const App = () => {
   const onChange = useCallback(
     (e) => {
       const { name, value } = e.target;
-      setForm({
-        ...form,
-        [name]: value,
-      });
+      setForm(
+        produce(form, (draft) => {
+          draft[name] = value;
+        })
+      );
     },
     [form]
   );
@@ -33,10 +35,11 @@ const App = () => {
         username: form.username,
       };
       // array에 새 항목 등록
-      setData({
-        ...data,
-        array: data.array.concat(info),
-      });
+      setData(
+        produce(data, (draft) => {
+          draft.array.push(info);
+        })
+      );
       // form 초기화
       setForm({
         name: "",
@@ -50,10 +53,14 @@ const App = () => {
   // 항목 삭제 하는 함수
   const onRemove = useCallback(
     (id) => {
-      setData({
-        ...data,
-        array: data.array.filter((info) => info.id !== id),
-      });
+      setData(
+        produce(data, (draft) => {
+          draft.array.splice(
+            draft.array.findIndex((info) => info.id === id),
+            1
+          );
+        })
+      );
     },
     [data]
   );
